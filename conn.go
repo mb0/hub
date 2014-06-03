c5559c6a302aa5271e9cfa93069fadb19d843f88
Martin Schnabel <mb0@mb0.org>
2014-06-04 01:56:42 +0200

    initial commit updated and extracted from github.com/mb0/lab/hub

---
+++ conn.go
@@ -0,0 +1,115 @@
+// Copyright 2014 Martin Schnabel. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package hub
+
+import (
+	"encoding/json"
+	"hash/fnv"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	writeWait  = 10 * time.Second
+	readWait   = 60 * time.Second
+	pingPeriod = (readWait * 9) / 10
+)
+
+type conn struct {
+	id     Id
+	send   chan Msg
+	wconn  *websocket.Conn
+	ticker *time.Ticker
+}
+
+func newconn(w http.ResponseWriter, r *http.Request) (*conn, error) {
+	wconn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if err != nil {
+		return nil, err
+	}
+	hash := fnv.New32()
+	hash.Write([]byte(r.RemoteAddr))
+	return &conn{Id(hash.Sum32()), make(chan Msg, 64), wconn, time.NewTicker(pingPeriod)}, nil
+}
+
+func (c *conn) read(h *Hub) {
+	for {
+		c.wconn.SetReadDeadline(time.Now().Add(readWait))
+		op, r, err := c.wconn.NextReader()
+		if err != nil {
+			if nerr, ok := err.(net.Error); !ok || !nerr.Timeout() {
+				switch err {
+				case io.EOF, io.ErrUnexpectedEOF:
+				default:
+					log.Printf("error receiving message %+v\n", err)
+				}
+				return
+			}
+		}
+		if op == websocket.BinaryMessage {
+			return
+		}
+		if op != websocket.TextMessage {
+			continue
+		}
+		bytes, err := ioutil.ReadAll(r)
+		if err != nil {
+			log.Println("error receiving message", err)
+			return
+		}
+		var msg Msg
+		err = json.Unmarshal(bytes, &msg)
+		if err != nil {
+			log.Println("error decoding message", err)
+			return
+		}
+		h.Route <- &Envelope{c.id, Route, msg}
+	}
+}
+
+func (c *conn) write() {
+	for {
+		select {
+		case msg, ok := <-c.send:
+			c.wconn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				c.wconn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			w, err := c.wconn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			enc := json.NewEncoder(w)
+			err = enc.Encode(msg)
+			if err != nil {
+				log.Println("error encoding message", err)
+			}
+			err = w.Close()
+			if err != nil {
+				log.Println("error sending message", err)
+				return
+			}
+		case now := <-c.ticker.C:
+			c.wconn.SetWriteDeadline(now.Add(writeWait))
+			err := c.wconn.WriteMessage(websocket.PingMessage, []byte{})
+			if err != nil {
+				log.Println("error sending ping", err)
+				return
+			}
+		}
+	}
+}
+
+func (c *conn) close() {
+	c.ticker.Stop()
+	c.wconn.Close()
+}
---
+++ hub.go
@@ -0,0 +1,173 @@
+// Copyright 2014 Martin Schnabel. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package hub
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Id int64
+
+func (id Id) MarshalJSON() ([]byte, error) {
+	str := fmt.Sprintf(`"%X"`, id)
+	return []byte(str), nil
+}
+func (id *Id) UnmarshalJSON(data []byte) error {
+	_, err := fmt.Sscanf(string(data), `"%X"`, id)
+	return err
+}
+
+const (
+	Route  Id = 0
+	Group  Id = 1 << 32
+	Except Id = 2 << 32
+)
+
+type Msg struct {
+	Head string
+	Data *json.RawMessage `json:",omitempty"`
+}
+
+func Marshal(head string, v interface{}) (m Msg, err error) {
+	var data []byte
+	data, err = json.Marshal(v)
+	if err != nil {
+		return
+	}
+	m.Head = head
+	m.Data = (*json.RawMessage)(&data)
+	return
+}
+
+func (m *Msg) Unmarshal(v interface{}) error {
+	var b []byte
+	if m.Data != nil {
+		b = []byte(*m.Data)
+	}
+	return json.Unmarshal(b, v)
+}
+
+var (
+	Signon  = "_signon"
+	Signoff = "_signoff"
+)
+
+type Envelope struct {
+	From, To Id
+	Msg
+}
+
+func (e Envelope) Reply(head string, v interface{}) (*Envelope, error) {
+	var err error
+	e.From, e.To = e.To, e.From
+	e.Msg, err = Marshal(head, v)
+	return &e, err
+}
+
+type Grouper interface {
+	GroupId() Id
+	Group() []Id
+}
+
+type Hub struct {
+	conns   map[Id]*conn
+	groups  map[Id]Grouper
+	signon  chan *conn
+	signoff chan *conn
+	Add     chan Grouper
+	Del     chan Grouper
+	Route   chan *Envelope
+	Send    chan *Envelope
+}
+
+func New() *Hub {
+	h := &Hub{
+		conns:   make(map[Id]*conn),
+		groups:  make(map[Id]Grouper),
+		signon:  make(chan *conn, 8),
+		signoff: make(chan *conn, 8),
+		Add:     make(chan Grouper, 8),
+		Del:     make(chan Grouper, 8),
+		Route:   make(chan *Envelope, 64),
+		Send:    make(chan *Envelope, 64),
+	}
+	go h.run()
+	return h
+}
+func (h *Hub) run() {
+	for {
+		select {
+		case c := <-h.signon:
+			h.conns[c.id] = c
+			h.Route <- &Envelope{c.id, Route, Msg{Head: Signon}}
+		case c := <-h.signoff:
+			delete(h.conns, c.id)
+			c.close()
+			close(c.send)
+			h.Route <- &Envelope{c.id, Route, Msg{Head: Signoff}}
+		case g := <-h.Add:
+			h.groups[g.GroupId()] = g
+		case g := <-h.Del:
+			delete(h.groups, g.GroupId())
+		case e := <-h.Send:
+			h.send(e)
+		}
+	}
+}
+
+func (h *Hub) send(e *Envelope) {
+	var except Id
+	if e.To&Except != 0 {
+		e.To ^= Except
+		except = e.From
+	}
+	switch {
+	case e.To == Route:
+		h.Route <- e
+	case e.To == Group:
+		for _, c := range h.conns {
+			if c.id != except {
+				c.send <- e.Msg
+			}
+		}
+	case e.To&Group != 0:
+		if g, ok := h.groups[e.To]; ok {
+			for _, to := range g.Group() {
+				if to == except {
+					continue
+				}
+				if c, ok := h.conns[to]; ok {
+					c.send <- e.Msg
+				}
+			}
+		}
+	default:
+		if c, ok := h.conns[e.To]; ok {
+			c.send <- e.Msg
+		}
+	}
+}
+
+func (h *Hub) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	c, err := newconn(w, r)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	select {
+	case h.signon <- c:
+		go c.write()
+		c.read(h)
+		h.signoff <- c
+	default:
+		http.Error(w, "Closing", http.StatusServiceUnavailable)
+	}
+}
